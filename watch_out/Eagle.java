ed4867a945d8ed65accc4422ad88de2d36b8d1f3
Nick Anstoetz <nanstoetz@lcs.on.ca>
2019-03-30 10:42:44 -0400

    I added pictures to every class I need

---
+++ watch_out/Eagle.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Eagle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Eagle extends Birds
+{
+    /**
+     * Act - do whatever the Eagle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
